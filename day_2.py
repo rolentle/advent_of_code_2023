6f40b3142b3e38caa43ba80d6f7372e5e387d6f2
Rolen Le <rolentle@gmail.com>
2023-12-02 09:59:39 -0500

    Day 2 part 2

--- day_2.py
+++ day_2.py
@@ -11,6 +11,15 @@ def day_2(input: str, bag_data: dict)-> int:
     
     return total
 
+def day_2_part_2(input: str)-> int:
+    power = 0
+    for game_text in input.split("\n"):
+        game_data = parse_day_2_line_data(game_text)
+        power += game_power(game_data)
+
+    return power
+
+
 
 def parse_day_2_line_data(input: str)-> dict:
     game_id_text, set_text = input.split(":")
@@ -41,6 +50,26 @@ def valid_day_2_game_data(game_data: dict, bag_data: dict)-> bool:
 
     return True
 
+def fewest_dice_possible(game_data: dict)-> dict:
+    fewest_dice_possible = {}
+    print(game_data)
+    for game_set in game_data["sets"]:
+        for dice_color in game_set:
+            if game_set[dice_color] > fewest_dice_possible.get(dice_color, 0):
+                fewest_dice_possible[dice_color] = game_set[dice_color]
+
+    return fewest_dice_possible
+
+
+def game_power(game_data: dict)-> int:
+    power = 1
+    for value in fewest_dice_possible(game_data).values():
+        power *= value
+
+    return power
+
+
+# Test
 f = open("./fixtures/day_2_sample.txt")
 day_2_sample_data = f.read()
 bag_data = {
@@ -124,3 +153,51 @@ def test_invalid_day_2_game_data():
 
     assert valid_day_2_game_data(expected_game_3_data, bag_data) == False
 
+def test_fewest_dice_possible():
+    expected_game_1_data = {
+        "id": 1,
+        "sets": [
+            {
+                "blue": 3,
+                "red": 4,
+            },
+            {
+                "red": 1,
+                "green": 2,
+                "blue": 6,
+            },
+            {
+                "green": 2,
+            }
+        ]
+    }
+    assert fewest_dice_possible(expected_game_1_data) == { "blue": 6, "red": 4, "green": 2}
+
+
+def test_game_power():
+    expected_game_1_data = {
+        "id": 1,
+        "sets": [
+            {
+                "blue": 3,
+                "red": 4,
+            },
+            {
+                "red": 1,
+                "green": 2,
+                "blue": 6,
+            },
+            {
+                "green": 2,
+            }
+        ]
+    }
+    assert game_power(expected_game_1_data) == 48
+
+def test_day_2_part_2_sample():
+    assert day_2_part_2(day_2_sample_data) == 2286
+
+def test_day_2_part_2():
+    f = open("./fixtures/day_2_part_1.txt")
+    day_2_data = f.read()
+    assert day_2_part_2(day_2_data) == 72970
